dbea379af35e2cf56803b293d955a47c64e18aba
vetrov129 <vetrov129@yandex.ru>
2021-11-14 00:32:23 +0300

    SearchUseCase

--- src/main/kotlin/com/example/as_example/favors/SearchRouter.kt
+++
@@ -1,2 +0,0 @@
-package com.example.as_example.favors
-
---
+++ src/main/kotlin/com/example/as_example/search/data_source/models/Stock.kt
@@ -0,0 +1,27 @@
+package com.example.as_example.search.data_source.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Stock(
+    var symbol: String,
+    var company: String,
+    var image: String,
+    var type: String,
+    val price: Price,
+) {
+    @Serializable
+    data class Price(
+        var value: Double,
+        var currency: String,
+        val differences: Difference
+    ) {
+        @Serializable
+        data class Difference(
+            var changesPercentage: Double,
+            var change: Double,
+            var isGrows: Boolean,
+            val period: String
+        )
+    }
+}
